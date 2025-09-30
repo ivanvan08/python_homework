3141a7098f6c1971840a458392e67fd211d4e5ec
Ivan Ulyanchenko <python0809999@gmail.com>
2025-09-30 09:32:11 +0300

    task 3 done

--- task03_exam_results_task.py
+++ task03_exam_results_task.py
@@ -14,9 +14,17 @@ OUTPUT EXAMPLE
 Average: 68.50
 Above average: [72, 88, 91]
 """
-
 grades = [45, 72, 88, 91, 60, 55]
 print("(Starter) Grades:", grades)
 # TODO: compute average via a loop (no sum())
+grades_sum = 0
+for i in grades:
+    grades_sum = grades_sum+i
+average = grades_sum/len(grades)
 # TODO: build a list of grades > average
+more_then_average = []
+for i in grades:
+    if i > average:
+        more_then_average.append(i)
 # TODO: print both
+print(average, more_then_average)
