8bfab3a3383de522343c6c8ef4099308dfb2b202
Ivan Ulyanchenko <python0809999@gmail.com>
2025-09-30 11:03:16 +0300

    task 4 done

--- task04_weather_converter_task.py
+++ task04_weather_converter_task.py
@@ -22,3 +22,10 @@ Fahrenheit list: [28, 39, 54, 64, 77]
 temps_c = [-2, 4, 12, 18, 25]
 print("(Starter) Celsius:", temps_c)
 # TODO: loop over temps_c, compute F, print each pair, and append rounded F to temps_f
+# F = C * (9/5) + 32
+Fahrenheit_list = []
+for cel in temps_c:
+    fahrenheit = round(cel * (9/5) + 32)
+    print(f"{cel}°C = {fahrenheit}°F")
+    Fahrenheit_list.append(fahrenheit)
+print(f"Fahrenheit list = {Fahrenheit_list}")
