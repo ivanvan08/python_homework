f64432063752fa0d491518c1023024dbafe24dac
Ivan Ulyanchenko <python0809999@gmail.com>
2025-10-04 20:41:38 +0300

    task 8 done

--- task08_movie_ratings_task.py
+++ task08_movie_ratings_task.py
@@ -19,3 +19,13 @@ Number of 5-star reviews: 3
 ratings = [4, 5, 3, 2, 5, 4, 5, 3, 4]
 print("(Starter) Ratings:", ratings)
 # TODO: compute average and number of 5-star reviews using loops only
+counter = 0
+average = 0
+perfect_reviews_counter = 0
+for i in ratings:
+    counter = counter + 1
+    average = average + i
+    if i == 5:
+        perfect_reviews_counter = perfect_reviews_counter + 1
+
+print(f"Average rating: {round(average/counter)}\nNumber of 5-star reviews: {perfect_reviews_counter}")
