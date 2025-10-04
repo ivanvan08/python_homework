2a401344884cc95cd77181fd79df456ea35e357e
Ivan Ulyanchenko <python0809999@gmail.com>
2025-10-04 20:22:20 +0300

    task 6 overdone without bugs

--- task06_warehouse_update_task.py
+++ task06_warehouse_update_task.py
@@ -14,10 +14,23 @@ OUTPUT EXAMPLE
 Inventory: [['laptops', 15], ['keyboards', 37], ['mice', 30]]
 keyboards: 40 -> 37
 """
-from re import split
 
 inventory = [["laptops", 15], ["keyboards", 40], ["mice", 30]]
 print("(Starter) Inventory:", inventory)
 # TODO: find "keyboards" row and subtract 3 (min 0), then print before/after
-a = str(inventory).split(",")
-print(a)
+whattobuy = input("what from the list do you want to buy ")
+howmany = input("how many? ")
+for i in inventory:
+    if whattobuy in str(i) and whattobuy != "," and whattobuy != "":
+        if int(howmany) <= i[1]:
+            print(f"{whattobuy}: {i[1]}->{i[1]-int(howmany)}")
+        else:
+            print("we don't have that amount of product")
+            wanttobuy = input("do you want to buy all what we have? (yes or no)")
+            if wanttobuy.lower() == "yes":
+                howmany = i[1]
+                print(f"{whattobuy}: {i[1]}->{i[1] - int(howmany)}. Thanks for buying all!!!")
+            elif wanttobuy.lower() == "no":
+                print("change your product or amount")
+                break
+            else: print("input yes or no next time")
