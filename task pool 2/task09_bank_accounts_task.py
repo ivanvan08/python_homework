0697f824ce414ffe8e4c508cc650a168f0eda4ee
Ivan Ulyanchenko <python0809999@gmail.com>
2025-10-10 20:30:09 +0300

    task 9 done (even with sort)

--- task pool 2/task09_bank_accounts_task.py
+++ task pool 2/task09_bank_accounts_task.py
@@ -9,7 +9,24 @@ Apply transactions (add deltas). If name not in accounts, create with starting b
 Finally, print balances sorted by name.
 
 """
+from tkinter.font import names
+
 # Starter:
 accounts = {"Alex": 1000, "Bob": 500, "Cara": 200}
-tx = [("Alex",-200), ("Bob",+300), ("Alex",+100), ("Cara",-50), ("Dan",+40)]
+tx = [("Alex",-200), ("Bob",+300), ("Alex",+100), ("Cara",-50), ("Dan",+40), ("VAN",+21341234123)]
 # TODO: apply tx to accounts; then print sorted balances
+for user in tx:
+    for obj in user:
+        if type(obj) is str:
+            if not obj in accounts:
+                print()
+                accounts.update({obj: user[1]})
+            if not obj in accounts:
+                accounts.update({obj: accounts[obj]+user[1]})
+        elif type(obj) is int:
+            break
+        else:
+            print("non valid object")
+            tx.pop()
+            break
+print(sorted(accounts.items(), key=lambda item: item[0]))
