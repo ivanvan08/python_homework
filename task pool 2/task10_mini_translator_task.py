785c9090896e623c73e4989a478f7c80be682c48
Ivan Ulyanchenko <python0809999@gmail.com>
2025-10-10 21:53:52 +0300

    task 10 done (in 1 min)

--- task pool 2/task10_mini_translator_task.py
+++ task pool 2/task10_mini_translator_task.py
@@ -9,3 +9,7 @@ Build a result list of translations (or "N/A" if no translation). Print the resu
 
 """
 # TODO: fill result with translations using .get; then print(result)
+dictionary = {"cat":"кіт", "dog":"собака", "bird":"пташка"}
+source_words = ["cat", "dog", "car", "bird"]
+for source in source_words:
+    print(dictionary.get(source))
