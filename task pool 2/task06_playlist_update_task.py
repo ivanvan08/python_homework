ffce03b5f0b9b9c3b4772753a41bf0b813cc735d
Ivan Ulyanchenko <python0809999@gmail.com>
2025-10-10 17:25:53 +0300

    task 6 done (with random)

--- task pool 2/task06_playlist_update_task.py
+++ task pool 2/task06_playlist_update_task.py
@@ -7,12 +7,21 @@ Playlist: {"rock": ["Queen", "AC/DC"], "pop": ["Adele"], "jazz": []}
 Add one new artist to each genre list, then print all genres and artists.
 
 """
+import random
+
 # Starter:
 playlist = {"rock": ["Queen", "AC/DC"], "pop": ["Adele"], "jazz": []}
 # TODO: append one artist per genre; then print genre -> list
-b = []
 for i in playlist:
-    a = playlist.get(i)
-    playlist.update(i)
+    a = playlist[i]
+    for c in range(3):
+        b = random.randint(1, 3)
+        if b == 1:
+            artist = "Mikel Jackson"
+        if b == 2:
+            artist = "Billie Eilish"
+        if b == 3:
+            artist = "Mozart"
+    playlist[i].append(artist)
 print(playlist)
 
