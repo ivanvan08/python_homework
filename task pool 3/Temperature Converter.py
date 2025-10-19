924a5577f4b674242878ed23c2c9ce112e1e137b
Ivan Ulyanchenko <python0809999@gmail.com>
2025-10-19 16:43:39 +0300

    Temperature Converter.py - step 2 done (without understanding task)

--- task pool 3/Temperature Converter.py
+++ task pool 3/Temperature Converter.py
@@ -1,10 +1,15 @@
 income  = [-10, -5, 0, 12.5, 23.1, 35, 41, 100, 250, 300, 420]
 fahrenheit = []
 celsius = []
-for C in income:
-    F = C*9/5+32
-    fahrenheit.append(F)
-    celsius.append(C)
+Far = None
+C = None
+def convert(C, Far, income):
+    for C in income:
+        Far = C*9/5+32
+        fahrenheit.append(Far)
+        celsius.append(C)
+    return
+convert(Far, C, income)
 print(f"""Celsius: {celsius}
 {"-" * sum(len(str(C))+3 for C in fahrenheit)}
 Fahrenheit: {fahrenheit}""")
