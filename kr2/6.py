a68bdca9fe509f249fdd5f97d7526381de632b42
Ivan Ulyanchenko <python0809999@gmail.com>
2025-11-21 21:36:24 +0200

    kr 2 - six - done (looking on the output, I changed point of getting upper few times)

--- kr2/6.py
+++ kr2/6.py
@@ -1,17 +1,18 @@
-inputer = input("ввести речення ").strip().split()
+raw_inputer = input("ввести речення ")
+inputer = raw_inputer.upper().strip().split()
 def loop():
     new_list = []
     for word in inputer:
         if len(word) <= 3:
-            new_list.append(word.upper())
+            new_list.append(word)
     return print(new_list)
 def comprehension():
-    new_list = [word.upper() for word in inputer if len(word) <= 3]
+    new_list = [word for word in inputer if len(word) <= 3]
     return print(new_list)
 choose = input("loop (1) or list comp. (2) ")
 if int(choose.strip()) == 1:
-    print(inputer, "->")
+    print(raw_inputer, "->")
     loop()
 elif int(choose.strip()) == 2:
-    print(inputer, "->")
+    print(raw_inputer, "->")
     comprehension()
