b2f213d24af26e66604a2ff0a5cf400d375252e1
Ivan Ulyanchenko <python0809999@gmail.com>
2026-01-16 19:08:58 +0200

    Ulyanchenko_1_N^2logN(2.0).py low - changes, finished

--- Ulyanchenko_1_N^2logN(2.0).py
+++ Ulyanchenko_1_N^2logN(2.0).py
@@ -8,8 +8,7 @@ negative_n = int(n)*(-1)
 numbers_data = random.sample(range(negative_n, n+1), n*2)
 numbers_data.sort()
 
-def binary_search(numbers, target):
-    low = 0
+def binary_search(numbers, target, low):
     high = len(numbers) - 1
     while low <= high:
         mid = (low + high) // 2
@@ -27,6 +26,6 @@ for i in range(0, n*2):
         if i > 0 and numbers_data[i] == numbers_data[i - 1]:
             continue
         c = -(numbers_data[i] + numbers_data[j])
-        if c in numbers_data:
+        if binary_search(numbers_data, c, j + 1):
             count += 1
 print(count)
