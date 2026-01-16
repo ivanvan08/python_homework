be5f801b92cf6178eddc79ffc139dd6a57d40b44
Ivan Ulyanchenko <python0809999@gmail.com>
2026-01-16 19:02:37 +0200

    Ulyanchenko_1_N^2logN(2.0).py binary_search + n^2longn optimisation for c

--- Ulyanchenko_1_N^2logN(2.0).py
+++ Ulyanchenko_1_N^2logN(2.0).py
@@ -6,13 +6,27 @@ solution_dict = dict.fromkeys(range(n))
 
 negative_n = int(n)*(-1)
 numbers_data = random.sample(range(negative_n, n+1), n*2)
-count = None
 numbers_data.sort()
+
+def binary_search(numbers, target):
+    low = 0
+    high = len(numbers) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if numbers[mid] == target:
+            return True
+        elif numbers[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return False
+
+count = 0
 for i in range(0, n*2):
-    for j in range(0, n*2):
+    for j in range(i+1, n*2):
         if i > 0 and numbers_data[i] == numbers_data[i - 1]:
             continue
         c = -(numbers_data[i] + numbers_data[j])
-        if numbers_data[i] + numbers_data[j] + c == 0:
+        if c in numbers_data:
             count += 1
 print(count)
