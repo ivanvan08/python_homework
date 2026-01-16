00fe381278f3b3bd4b9bc3c023a946c03e38472c
Ivan Ulyanchenko <python0809999@gmail.com>
2026-01-16 18:09:44 +0200

    Ulyanchenko_1_N^2logN(2.0).py sort + binary search of third number logic (beta)

--- Ulyanchenko_1_N^2logN(2.0).py
+++ Ulyanchenko_1_N^2logN(2.0).py
@@ -6,9 +6,13 @@ solution_dict = dict.fromkeys(range(n))
 
 negative_n = int(n)*(-1)
 numbers_data = random.sample(range(negative_n, n+1), n*2)
-count = 0
+count = None
+numbers_data.sort()
 for i in range(0, n*2):
-    for j in range(i+1, n*2):
-        if numbers_data[i] + numbers_data[j] == 0:
+    for j in range(0, n*2):
+        if i > 0 and numbers_data[i] == numbers_data[i - 1]:
+            continue
+        c = -(numbers_data[i] + numbers_data[j])
+        if numbers_data[i] + numbers_data[j] + c == 0:
             count += 1
 print(count)
