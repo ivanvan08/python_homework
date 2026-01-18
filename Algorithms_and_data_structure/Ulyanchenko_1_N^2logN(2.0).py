49d3fc36cb85e39ea2d23b8ac988d0fc94d28b04
Ivan Ulyanchenko <python0809999@gmail.com>
2026-01-18 20:07:09 +0200

    files restructure, PEP 8 changes

--- Algorithms_and_data_structure/Ulyanchenko_1_N^2logN(2.0).py
+++ Algorithms_and_data_structure/Ulyanchenko_1_N^2logN(2.0).py
@@ -4,9 +4,10 @@ n = int(input("Введіть кількість цілих чисел "))
 
 solution_dict = dict.fromkeys(range(n))
 
-negative_n = int(n)*(-1)
-numbers_data = random.sample(range(negative_n, n+1), n*2)
+negative_n = int(n) * (-1)
+numbers_data = random.sample(range(negative_n, n + 1), n * 2)
 numbers_data.sort()
+
 
 def binary_search(numbers, target, low):
     high = len(numbers) - 1
@@ -20,9 +21,10 @@ def binary_search(numbers, target, low):
             high = mid - 1
     return False
 
+
 count = 0
-for i in range(0, n*2):
-    for j in range(i+1, n*2):
+for i in range(0, n * 2):
+    for j in range(i + 1, n * 2):
         if i > 0 and numbers_data[i] == numbers_data[i - 1]:
             continue
         c = -(numbers_data[i] + numbers_data[j])
