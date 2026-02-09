3b1e4e009ff7840ccf7daa6bd8daad20e59a2ff6
Ivan Ulyanchenko <python0809999@gmail.com>
2026-02-09 20:44:42 +0200

    insertion_sort.py update for merge_sort.py

--- Algorithms_and_data_structure/second_practice/insertion_sort.py
+++ Algorithms_and_data_structure/second_practice/insertion_sort.py
@@ -1,9 +1,9 @@
-def insertion_sort(array):
-    for step in range(1, len(array)):
+def insertion_sort(array, lo, hi):
+    for step in range(lo + 1, hi + 1):
         key = array[step]
         j = step - 1
 
-        while j >= 0 and key < array[j]:
+        while j >= lo and key < array[j]:
             array[j + 1] = array[j]
             j = j - 1
 
@@ -12,6 +12,6 @@ def insertion_sort(array):
 
 if __name__ == '__main__':
     data = [9, 5, 1, 4, 3]
-    insertion_sort(data)
+    insertion_sort(data, 0, len(data) + 1)
     print('Sorted Array in Ascending Order:')
     print(data)
