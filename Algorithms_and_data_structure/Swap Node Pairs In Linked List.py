7fbf524bd1bd03f444191ee85a90fb04f5ad04af
Ivan Ulyanchenko <python0809999@gmail.com>
2026-03-21 19:30:56 +0200

    8 week homework

---
+++ Algorithms_and_data_structure/Swap Node Pairs In Linked List.py
@@ -0,0 +1,10 @@
+public class Node {
+    private String value;
+    public Node next;
+
+    public Node(String value) { this.value = value; }
+
+    public String getValue() { return value; }
+  // returns a String representation of the whole list:
+    public String printList() {}
+}
