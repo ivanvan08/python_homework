bbdf825027ab8f46dad542b1bb33a4d09724a662
Ivan Ulyanchenko <python0809999@gmail.com>
2026-03-30 20:02:49 +0300

    leaking - init, _find, _union, open, _pick_random - done

--- Algorithms_and_data_structure/second_practice/percolation.py
+++ Algorithms_and_data_structure/second_practice/percolation.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Percolation:
     def __init__(self, number: int):
         """
@@ -5,14 +8,22 @@ class Percolation:
         :param number: <int> number of rows and columns
         """
         self.matrix = [[False for j in range(number)] for k in range(number)]
-        self.parent = [j for j in range(number) + 2]
+        self.linked = [j for j in range(number * number + 2)]
         self.top = number * number
         self.bottom = number * number + 1
-        for j in range(number):
-            union(TOP, j)
+        self.number = number
 
-        for j in range(number):
-            union(BOTTOM, number * (number - 1) + j)
+    def _find(self, x):
+        current = x
+        while self.linked[current] != current:
+            current = self.linked[current]
+        return current
+
+    def _union(self, x, y):
+        parent_a = self._find(x)
+        parent_b = self._find(y)
+        self.linked[parent_a] = parent_b
+        return True
 
     def opened_count(self) -> int:
         """
@@ -25,7 +36,30 @@ class Percolation:
         """
         open random cell if it is not opened yet
         """
-        ...
+        cell = self._pick_random()
+        self.matrix[cell[0]][cell[1]] = True
+        new_index = cell[0] * self.number + cell[1]
+        if cell[0] == 0:
+            self._union(new_index, self.top)
+        elif cell[0] == self.number - 1:
+            self._union(new_index, self.bottom)
+        neighbours = [
+            (cell[0] - 1, cell[1]),
+            (cell[0] + 1, cell[1]),
+            (cell[0], cell[1] - 1),
+            (cell[0], cell[1] + 1)
+        ]
+        for n_i, n_j in neighbours:
+            if 0 <= n_i < self.number and 0 <= n_j < self.number:
+                if self.matrix[n_i][n_j] is True:
+                    self._union(n_i * self.number + n_j, new_index)
+
+    def _pick_random(self):
+        cell = random.randint(0, self.number - 1), random.randint(0, self.number - 1)
+        if not self.matrix[cell[0]][cell[1]]:
+            return cell
+        else:
+            return self._pick_random()
 
     def is_opened(self, i: int, j: int) -> bool:
         """
