6abdecc9889c9a5c6d386b1003cc81477152b250
Ivan Ulyanchenko <python0809999@gmail.com>
2026-04-13 21:17:35 +0300

    lection 13 Ulyanchenko_13_Kosaradge_algorithm.py - done with tests

--- Algorithms_and_data_structure/Ulyanchenko_13_Kosaradge_algorithm.py
+++ Algorithms_and_data_structure/Ulyanchenko_13_Kosaradge_algorithm.py
@@ -47,3 +47,26 @@ def kosaradge(g):
                     marked[w] = True
             result.append(scc)
     return result
+
+
+if __name__ == "__main__":
+    g = digraph.DiGraph()
+    g.add_edge(0, 1)
+    g.add_edge(0, 5)
+    g.add_edge(2, 0)
+    g.add_edge(2, 3)
+    g.add_edge(3, 5)
+    g.add_edge(3, 2)
+    g.add_edge(4, 3)
+    g.add_edge(4, 2)
+    g.add_edge(5, 4)
+    g.add_edge(6, 8)
+    g.add_edge(6, 9)
+    g.add_edge(7, 6)
+    g.add_edge(8, 6)
+    g.add_edge(9, 10)
+    g.add_edge(9, 11)
+    g.add_edge(10, 12)
+    g.add_edge(11, 12)
+    g.add_edge(12, 9)
+    print(kosaradge(g))
